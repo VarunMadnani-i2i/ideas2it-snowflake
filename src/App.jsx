65bcd66e4cb222c726515c5abc7ec4e036484fbd
chintankamani <chintanpkamani@gmail.com>
2024-05-29 14:57:54 +0530

    sa

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import WarehouseManager from './components/WarehouseManager';
 import WarehouseForm from './components/WarehouseForm';
 import './App.css';
@@ -13,8 +13,35 @@ function App() {
   };
 
   const handleHubspotSubmission = () => {
-    setCurrentView('warehouseManager');
+    // setTimeout(() => {
+      setCurrentView('warehouseManager');
+    // },1500)
+    
   };
+
+  useEffect(() => {
+    if (currentView === 'hubspot') {
+      const script = document.createElement('script');
+      script.src = 'https://js.hsforms.net/forms/embed/v2.js';
+      document.body.appendChild(script);
+
+      script.addEventListener('load', () => {
+        if (window.hbspt) {
+          window.hbspt.forms.create({
+            region: "na1",
+            portalId: "24031861",
+            formId: "e2857982-a11e-494b-a5e3-cae149755208",
+            onFormSubmitted: handleHubspotSubmission,
+          });
+        }
+      });
+
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
+  }, [currentView]);
+
 
   return (
     <section className="flex flex-col max-w-[1100px] p-4 mx-auto">
@@ -25,17 +52,7 @@ function App() {
         <div>
           {/* Embed your HubSpot form here */}
           <h2>Submit the form below to proceed</h2>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleHubspotSubmission();
-            }}
-          >
-            {/* Replace with your actual HubSpot form */}
-            <input type="text" placeholder="Name" required />
-            <input type="email" placeholder="Email" required />
-            <button type="submit">Submit</button>
-          </form>
+          <div id="hubspotForm"></div>
         </div>
       )}
       {currentView === 'warehouseManager' && (
